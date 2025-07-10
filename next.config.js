a36d4a9452047673c91b868916d6a342ff7e1215
Rauljouman <rauljouman@gmail.com>
2025-07-11 01:14:58 +0200

    Cambios para subir

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);
