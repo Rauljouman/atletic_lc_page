7d91640b42d3121b7dc4143c781b839d0ef29876
Rauljouman <rauljouman@gmail.com>
2025-07-11 01:51:59 +0200

    cambio a 1 foto

--- src/components/GroupSection.jsx
+++ src/components/GroupSection.jsx
@@ -1,34 +1,19 @@
-import { useEffect, useState } from "react";
 import styles from "./GroupSection.module.css";
 import Image from "next/image";
 
-const images = [
-  { src: "/assets/SeniorA_photo.jpg", alt: "Equipo Sénior A del Atletic Les Corts Futsal" },
-  { src: "/assets/JuvenilA_photo.jpg", alt: "Equipo Juvenil A del Atletic Les Corts Futsal" },
-];
-
 export default function GroupSection() {
-  const [currentImage, setCurrentImage] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <section className={styles.groupSection}>
       <h1 className={styles.title}>Atletic Les Corts</h1>
       <div className={styles.photoBox}>
         <Image
-          src={images[currentImage].src}
-          alt={images[currentImage].alt}
-          style={{ objectFit: "cover" }}
-          className={styles.photo}
+          src="/assets/SeniorA_photo.jpg"
+          alt="Equipo Sénior A del Atletic Les Corts Futsal"
           fill
-          priority={true}
+          className={styles.photo}
+          style={{ objectFit: "cover" }}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 800px"
+          priority
           fetchPriority="high"
         />
       </div>
