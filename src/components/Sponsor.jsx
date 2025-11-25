a6dc35f8c9f249f7e7d47bd3bf60e195a66d6fbc
Raul Ip <rauljouman@gmail.com>
2025-11-25 16:10:39 +0100

    fix img

--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sponsor.module.css";
+import React from "react";
 
 export default function Sponsor() {
   return (
@@ -13,35 +14,35 @@ export default function Sponsor() {
       <div className={styles.sponsors}>
 
         <a href="https://www.instagram.com/bocaarte.gastrotapas/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/Bocaarte.png" alt="BocaArte" />
+          <img src="/assets/Sponsors/Bocaarte.png" alt="BocaArte" />
         </a>
 
         <a href="https://www.instagram.com/vitosport_bcn/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/vito.png" alt="VitoSport" />
+          <img src="/assets/Sponsors/vito.png" alt="VitoSport" />
         </a>
 
         <a href="https://www.instagram.com/reketepizza_/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/reketepizza.jpg" alt="ReketePizza" />
+          <img src="/assets/Sponsors/reketepizza.jpg" alt="ReketePizza" />
         </a>
 
         <a href="https://www.instagram.com/movment.bcn/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/movment.jpg" alt="Movment" />
+          <img src="/assets/Sponsors/movment.jpg" alt="Movment" />
         </a>
 
         <a href="https://www.instagram.com/bcoach.app/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/bcoach.jpg" alt="bcoach" />
+          <img src="/assets/Sponsors/bcoach.jpg" alt="bcoach" />
         </a>
 
         <a href="https://www.instagram.com/zumub.es/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/zumub.jpg" alt="Zumub" />
+          <img src="/assets/Sponsors/zumub.jpg" alt="Zumub" />
         </a>
 
         <a href="https://www.instagram.com/zionsportsanalytics/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/zion.png" alt="Zion Analytics" />
+          <img src="/assets/Sponsors/zion.png" alt="Zion Analytics" />
         </a>
 
         <a href="https://www.instagram.com/cluber_app/" target="_blank" rel="noopener noreferrer">
-          <img src="/assets/sponsors/cluber.png" alt="Cluber" />
+          <img src="/assets/Sponsors/cluber.png" alt="Cluber" />
         </a>
 
       </div>
